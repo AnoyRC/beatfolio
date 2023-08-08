cb638b13ef5390fcf5d4882afa50223e911ab56a
Gautam Raj <ashragautam25@gmail.com>
2023-08-08 21:53:58 +0530

    - Add Music modal
    - Add music modal
    - Add back icon
    - Add prevent select class in tailwind config
    - Update music player

--- Frontend/app/page.js
+++ Frontend/app/page.js
@@ -1,12 +1,14 @@
 import { MusicPlayer, ModalMusicPlayer } from '@/components/MusicPlayer';
+import MusicModal from '@/components/modal/MusicModal';
 import Nav from '@/components/navigation/Nav';
 
 export default function Home() {
   return (
     <main className="h-screen flex max-w-7xl relative">
       <Nav />
-      <MusicPlayer />
+      {/* <MusicPlayer /> */}
       {/* <ModalMusicPlayer /> */}
+      {/* <MusicModal /> */}
     </main>
   );
 }
