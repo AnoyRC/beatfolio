2f0232d5807a1de9de027f0a011d3123b76de5d4
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-08-22 15:07:36 +0530

    Fixed Get Started Now route

--- Frontend/components/layout/home/Header.jsx
+++ Frontend/components/layout/home/Header.jsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const header = () => {
   const router = useRouter();
@@ -11,18 +11,18 @@ const header = () => {
       <div className="py-36">
         <h1 className="text-7xl leading-tight font-bold">
           <span className="block">Unleash Your</span>
-          <span className="text-gradiant">{' Musical Creativity'}</span>
+          <span className="text-gradiant">{" Musical Creativity"}</span>
         </h1>
 
         <p className="text-gray-500 max-w-xs mt-2">
           The future of music streaming platform that gives you the power to
-          build a more equitable{' '}
+          build a more equitable{" "}
           <span className="text-gradiant">Music Industry.</span>
         </p>
 
         <button
           className="px-6 py-3 btn-gradiant rounded-full cursor-pointer font-bold hover:scale-105 active:scale-95 transition-transform duration-150 mt-16"
-          onClick={() => router.push('/feed')}
+          onClick={() => router.push("/users/home")}
         >
           Get Started Now
         </button>
